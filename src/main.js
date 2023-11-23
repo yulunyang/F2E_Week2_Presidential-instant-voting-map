e15e8edb8bfe64762c30a4ae9ee7e5e2d7ab31f3
crystal <yulunyangfrontend@gmail.com>
2023-11-23 11:36:51 +0800

    remove useless packagw

--- src/main.js
+++ src/main.js
@@ -5,8 +5,6 @@ import store from './store'
 import './index.css'
 import '@/assets/main.scss'
 import { gsap, ScrollTrigger, Draggable, MotionPathPlugin, TextPlugin } from "gsap/all";
-import AOS from 'aos'
-import 'aos/dist/aos.css'
 import $ from 'jquery'
 
 window.jQuery = $
@@ -15,6 +13,5 @@ window.$ = $
 gsap.registerPlugin(gsap, ScrollTrigger, Draggable, MotionPathPlugin, TextPlugin)
 
 export const app = createApp(App)
-app.AOS = new AOS.init()
 
-app.use(store).use(router).use(AOS).mount('#app')
+app.use(store).use(router).mount('#app')
